836a86da1e7f3d952da4d61ec5d906b096df532d
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-09-16 08:57:23 +0530

    Added Header, Balance, Add Trasaction components.

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,11 +1,64 @@
 //  Add Transaction Component
 
 // Imports
-import React from "react";
+import React, { useState, useContext } from "react";
+
+// Importing Global Context
+import { GlobalContext } from "../context/GlobalState";
 
 // Functional Component
 const AddTransaction = () => {
-  return <div>Add Transaction</div>;
+  // State Variables
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  // Context Variable
+  const { addTransaction } = useContext(GlobalContext);
+
+  // On Submit Handler
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount,
+    };
+
+    addTransaction(newTransaction);
+  };
+
+  return (
+    <>
+      <h3>Add new transaction</h3>
+
+      {/* Form */}
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text..."
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br /> (Negative - expense, Positive - income)
+          </label>
+
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Enter amount..."
+          />
+        </div>
+        <button className="btn">Add Transaction</button>
+      </form>
+    </>
+  );
 };
 
 // Default Export
--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,11 +1,42 @@
 // Balance Component
 
-// Imports
-import React from "react";
+// Importing Helpers
+import React, { useContext } from "react";
+
+// Importing Global Context
+import { GlobalContext } from "../context/GlobalState";
+
+// Money Formatting Function
+function moneyFormatter(num) {
+  let p = num.toFixed(2).split(".");
+
+  return (
+    "₹" +
+    p[0]
+      .split("")
+      .reverse()
+      .reduce((acc, num, i) => {
+        return num === "-" ? acc : num + (i && !(1 % 3) ? "," : "") + acc;
+      }, "") +
+    "." +
+    p[1]
+  );
+}
 
 // Functional Component
 const Balance = () => {
-  return <div>Balance</div>;
+  // Context Variable
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+  const total = amounts.reduce((acc, item) => (acc, item), 0);
+
+  return (
+    <>
+      <h4>Your Balance</h4>
+      <h1>{moneyFormatter(total)}</h1>
+    </>
+  );
 };
 
 // Default Export
